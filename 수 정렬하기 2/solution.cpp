445e0f2b70751cc8384c23456080cbd790eca10c
koreandroid <dlwotjd116@snu.ac.kr>
2025-01-28 21:28:02 +0900

    Create solution.cpp
    https://www.acmicpc.net/problem/2751

---
+++ 수 정렬하기 2/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n;
+
+    // Get an input for the value N, which is between 1 and 1000000.
+    scanf("%d", &n);
+
+    bool *numbersCheck = new bool[2000001];
+    for (int i = 0; i < n; i++) {
+        int number;
+        scanf("%d", &number);
+
+        numbersCheck[number + 1000000] = true;
+    }
+
+    for (int num = -1000000; num <= 1000000; num++) {
+        if (numbersCheck[num + 1000000]) {
+            printf("%d\n", num);
+        }
+    }
+
+    delete[] numbersCheck;
+
+    return 0;
+}
