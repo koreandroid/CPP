d075489daf1095f7d4bf3066434c42e54b4a3eba
koreandroid <dlwotjd116@snu.ac.kr>
2025-03-21 22:08:07 +0900

    Update solution.cpp

--- 연탄 배달의 시작/solution.cpp
+++ 연탄 배달의 시작/solution.cpp
@@ -8,8 +8,8 @@ int main()
     int n;
     scanf("%d", &n);
 
-    if (!(1 <= n && n <= N_MAX)) {
-        printf("The value of N is invalid. It should be between 1 and %d.\n", N_MAX);
+    if (!(2 <= n && n <= N_MAX)) {
+        printf("The value of N is invalid. It should be between 2 and %d.\n", N_MAX);
         return -1;
     }
 
