ab3b9cd7a87ccd48e4f832a325301ca360a40305
koreandroid <dlwotjd116@snu.ac.kr>
2025-03-21 21:49:50 +0900

    Create solution.cpp
    softeer.ai

---
+++ 연탄 배달의 시작/solution.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <algorithm>
+
+#define N_MAX 1000
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    if (!(1 <= n && n <= N_MAX)) {
+        printf("The value of N is invalid. It should be between 1 and %d.\n", N_MAX);
+        return -1;
+    }
+
+    unsigned * const locations = new unsigned[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%u", &locations[i]);
+    }
+
+    int min = locations[1] - locations[0];
+    unsigned count = 1;
+    for (int i = 2; i < n; i++) {
+        int dist = locations[i] - locations[i - 1];
+        if (dist < min) {
+            min = dist;
+            count = 1;
+        } else if (dist == min) {
+            count++;
+        }
+    }
+
+    printf("%u", count);
+
+    delete[] locations;
+
+    return 0;
+}
