d5018be2f6d69a169e75f3f6a1d82f3c0f50a785
koreandroid <dlwotjd116@snu.ac.kr>
2026-04-30 09:39:17 +0900

    Create solution.cpp
    programmers.co.kr

---
+++ 바탕화면 정리/solution.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    int lurow = wallpaper.size() - 1;
+    int lucol = wallpaper[0].length() - 1;
+    int rdrow = 0;
+    int rdcol = 0;
+
+    for (int i = 0; i < wallpaper.size(); i++) {
+        for (int j = 0; j < wallpaper[i].length(); j++) {
+            if (wallpaper[i][j] == '#') {
+                lurow = (i < lurow) ? i : lurow;
+                lucol = (j < lucol) ? j : lucol;
+                rdrow = (i > rdrow) ? i : rdrow;
+                rdcol = (j > rdcol) ? j : rdcol;
+            }
+        }
+    }
+
+    return {lurow, lucol, rdrow + 1, rdcol + 1};
+}
